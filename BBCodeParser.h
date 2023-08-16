b472f9f9ef606e1cb913ad40b7084766d7c9ce2c
Iddodo <m.ido@campus.technion.ac.il>
2023-08-16 23:41:48 +0300

    Deleted in the process of rewriting the project.

--- BBCodeParser.h
+++ BBCodeParser.h
@@ -1,129 +1,30 @@
-#ifndef BBCODEPARSER_H_
-#define BBCODEPARSER_H_
+#ifndef BBCODEPARSER_H
+#define BBCODEPARSER_H
 
+#include "BBCodeEntity.h"
+#include "BBCodeRenderer.h"
+#include "BBCodeRoot.h"
+#include "BBCodeStateManager.h"
 #include "BBCodeTag.h"
 
-
-#include <string>
-#include <stack>
 #include <memory>
-
-
-using std::string;
-using std::stack;
-using std::shared_ptr;
-
-#define LBRACKET '['
-#define RBRACKET ']'
-
+#include <stack>
 
 class BBCodeParser {
-
-    shared_ptr<BBCodeTag> root;
-    stack< shared_ptr<BBCodeTag> > tag_stack;
-
-    bool lbracket_identified;
+  BBCodeStateManager fsm;
+  BBCodeRenderer &renderer;
+  std::stack<std::unique_ptr<BBCodeNestable>> stack_nestables;
+  std::string buffer;
+  std::unique_ptr<BBCodeTag> active_tag;
+  std::string active_param_key;
+  std::string active_param_value;
 
 public:
-
-    BBCodeParser(string text) : root(make_shared<BBCodeTag>()), lbracket_identified(false) {
-        root->setRoot();
-        tag_stack.push(root);
-
-        // Iterate over text characters individually.
-
-        for (unsigned int i = 0; i < text.length(); i++) {
-
-            this->insertChar(text[i]);
-        }
-    }
+  BBCodeParser(std::string_view src_text, BBCodeRenderer &&rend);
 
 private:
-
-    void insertChar(char c) {
-
-        shared_ptr<BBCodeTag> controller = this->controllerTag();
-
-        // Handle the beginning of a new BBCode tag
-        if (c == LBRACKET) {
-
-            shared_ptr<BBCodeTag> new_tag = make_shared<BBCodeTag>(controller);
-            this->controllerTag()->pushContent(new_tag);
-            this->pushController(new_tag);
-
-            this->lbracket_identified = true;
-
-            return;
-        }
-
-        // Handle the ending of a BBCode tag
-        if (c == RBRACKET) {
-
-            // Prevent end case where someone might insert multiple rbrackets
-            if (!this->lbracket_identified) {
-                controller->insertChar(c);
-                return;
-            }
-
-            lbracket_identified = false;
-
-            if (controller->isClosingTag()) {
-
-                shared_ptr<BBCodeTag> parent = controller->getParent();
-
-                /* Tag mismatch with parents
-                 * Current approach: simply deem mismatched closing tag invalid */
-                if (!controller->matches(parent)) {
-
-                    controller->setInvalid(BBCODE_OUT_OF_PLACE_CLOSING_TAG);
-                }
-
-                // Otherwise, pop back BBCode content, as a valid closing tag
-                // should not be nested inside.
-
-               this->popController();
-               this->popController();
-
-               return;
-
-            }
-
-            if (controller->emptyTag()) {
-
-                this->popController();
-                return;
-            }
-
-            // Not a closing tag, exit param mode
-
-            controller->exitParamMode();
-            return;
-        }
-
-        // Identify a closing tag
-        if (c == '/' && controller->emptyTag()) {
-            controller->setClosing();
-            return;
-        }
-
-        controller->insertChar(c);
-
-    }
-
-private:
-
-    shared_ptr<BBCodeTag> controllerTag() { return this->tag_stack.top(); }
-
-    void pushController(shared_ptr<BBCodeTag> tag) { this->tag_stack.push(tag); }
-    void popController() {this->tag_stack.pop(); }
-
-// Render
-public:
-    string render() const {
-        return this->root->render();
-    }
-
-
+  void appendTextToCurrentNestable(std::string_view text);
+  std::unique_ptr<BBCodeNestable> &currentNestable();
 };
 
-#endif // BBCODEPARSER_H_
+#endif
--- BBCodeTag.h
+++ BBCodeTag.h
@@ -1,220 +1,30 @@
-#ifndef BBCODETAG_H_
-#define BBCODETAG_H_
+#ifndef BBCODETAG_H
+#define BBCODETAG_H
 
-#include "BBCodeTagInvalidness.h"
-#include "BBCodeContentObject.h"
-#include "BBCodeString.h"
-
-
-#include <vector>
+#include <list>
 #include <memory>
-#include <string>
-#include <memory>
-#include <cctype>
+#include <optional>
+#include <unordered_map>
 
+#include "BBCodeEntity.h"
+#include "BBCodeNestable.h"
 
-#include <iostream>
-
-
-using std::vector;
-using std::shared_ptr;
-using std::string;
-
-using std::shared_ptr;
-using std::make_shared;
-
-class BBCodeTag : public BBCodeContentObject {
-    bool param_mode;
-
-    BBCodeTagInvalidness valid;
-
-    shared_ptr<BBCodeTag> parent;
-
-    vector< shared_ptr<BBCodeContentObject> > inner;
-    vector<string> params;
-
-    bool root;
-
-    bool whitespace_sequence;
-
-    bool closing;
-
-// Insert into tag
-public:
-    void insertChar(char c) {
-
-        if (this->paramMode()) {
-
-            this->insertCharParam(c);
-            return;
-        }
-
-        this->insertCharInner(c);
-    }
-    
-    void pushContent(shared_ptr<BBCodeContentObject> content) {
-        this->inner.push_back(content);
-    }
-
-private:
-    void insertCharParam(char c) {
-
-        if (this->isWhitespace(c)) {
-            this->whitespace_sequence = true;
-            return;
-        }
-
-        // In case of an empty param list or a white sequence
-        if (this->params.empty() || this->whitespace_sequence) {
-            this->params.push_back("");
-        }
-
-        this->whitespace_sequence = false;
-        this->params.back() += c;
-    }
-
-    void insertCharInner(char c) {
-
-        // If last content object inside tag is not a string,
-        // Create a new empty string and push back to inner vector
-        if (this->inner.empty() || !(this->inner.back()->isString())) {
-
-            this->inner.push_back(make_shared<BBCodeString>());
-        }
-
-        this->inner.back()->insertChar(c);
-    }
-
-
-
-
-// Constructors
-public:
-    BBCodeTag(shared_ptr<BBCodeTag> parent = nullptr)
-        : param_mode(true), valid(BBCODE_VALID), parent(parent),
-          root(false), whitespace_sequence(false), closing(false){}
-
-// Boolean tests
-public:
-    bool isValid() const { return this->valid == BBCODE_VALID; }
-
-    bool paramMode() const { return this->param_mode; }
-
-    bool isString() const { return false; }
-
-    bool isClosingTag() const {
-        return this->closing;
-    }
-
-
-
-    bool matches(shared_ptr<BBCodeTag> tag2) {
-
-        if (!tag2 || this->emptyTag() || tag2->emptyTag()) return false;
-
-
-        return this->params[0].compare(tag2->params[0]) == 0;
-    }
-
-
-        bool emptyTag() const { return this->params.empty() || this->params[0].empty(); }
-
-
-
-// Getters and setters
-public:
-    shared_ptr<BBCodeTag> getParent() { return this->parent; }
-
-    void setInvalid(BBCodeTagInvalidness s) { this->valid = s; }
-
-
-    void exitParamMode() { this->param_mode = false; }
-
-    // Make this a friend function
-    void setRoot() {
-
-        this->root = true;
-        this->exitParamMode();
-    }
-
-    string name() {
-        // Empty tag
-        if (this->root) return "root";
-        if (this->emptyTag()) return "";
-
-        return this->params[0];
-    }
-
-    void setClosing() {
-        this->closing = true;
-    }
-
-private:
-
-    bool isWhitespace(char c) const {
-        return std::isspace(c);
-    }
-
-
-
-// Rendering function
+class BBCodeTag : public BBCodeEntity, public BBCodeNestable {
+  std::unordered_map<std::string_view, std::string_view> m_parameters;
+  std::string_view m_symbol;
 
 public:
+  BBCodeTag(std::string_view tag_symbol);
+  bool hasParams() const;
+  std::string_view &symbol() const;
 
-    string render() {
+  std::optional<std::string_view &> getParam(const std::string_view &key) const;
 
-        // Return nothing on empty tag
-        if(this->emptyTag() && !this->root) return "[]";
+  void setParam(const std::string &key, const std::string &val);
 
-        // Handle invalid tag
-        if (!this->isValid()) {
-            switch (this->valid) {
+  const auto &parameters() const;
 
-                case BBCODE_OUT_OF_PLACE_CLOSING_TAG:
-                    return "[/" + this->params[0] + "]";
-
-                default:
-                    return "";
-            }
-        }
-
-        // Return nothing on a valid closing tag
-        if (this->isClosingTag()) return "";
-
-        string inner = this->renderInner();
-        string tag = this->name();
-
-        if (tag == "b")
-            return "<b>" + inner + "</b>";
-
-        if (tag == "img")
-            return "<img src=\"" + inner + "\" alt=\"\" />";
-
-
-        if (tag == "u")
-            return "<u>" + inner + "</u>";
-
-        if (tag == "center")
-            return "<div style=\"text-align: center;\">" + inner + "</div>";
-
-
-        return inner;
-
-    }
-
-private:
-
-        string renderInner() {
-
-            string result = "";
-
-            for (shared_ptr<BBCodeContentObject> content : this->inner) {
-                result += content->render();
-            }
-
-            return result;
-        }
-
+  std::optional<std::string_view> nestableSymbol() const override;
 };
 
-#endif // BBCODETAG_H_
+#endif
